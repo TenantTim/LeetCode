9aa23a4d23a102c8cc655f120a7a5503574f81f2
Tim Zhang <weizhazh@microsoft.com>
2018-12-02 01:47:42 +0800

    Add a basic solution to the LeetCode vs solution

---
+++ LeetCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCode
+{
+   // This main program intentionally does nothing
+   // Only to pass the build
+   public class Program
+   {
+      static void Main( string[] args )
+      {
+      }
+   }
+}
