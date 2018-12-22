981c259146dd2224cf604e9cd3de62533c4dcb07
TenantTim <t-i.m@outlook.com>
2018-12-22 13:12:35 +0800

    Add solution for 138, modifiy ListNode a little to fit RandomListNode

--- LeetCode/Utils/ListNode.cs
+++ LeetCode/Utils/ListNode.cs
@@ -6,8 +6,8 @@ namespace LeetCode.Utils
 {
    public class ListNode
    {
-      public int val;
-      public ListNode next;
+      public int val { get; set; }
+      public ListNode next { get; set; }
 
       public ListNode( int x )
       {
---
+++ LeetCode/Utils/RandomListNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Utils
+{
+   public class RandomListNode : ListNode
+   {
+      public int label { get { return val; } }
+
+      public new RandomListNode next { get; set; }
+
+      public RandomListNode random { get; set; }
+
+      public RandomListNode( int x ) :
+         base( x )
+      {
+      }
+   }
+}
