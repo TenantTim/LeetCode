2000c0a0820e1533cce5a3185072994178e0fcf1
Tim Zhang <weizhazh@microsoft.com>
2019-09-06 12:29:32 -0700

    Add solution for LC226, 240, 350 and TreeNode util

---
+++ LeetCode/Solutions/LC226/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using LeetCode.Utils;
+
+namespace LeetCode.Solutions.LC226
+{
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int x) { val = x; }
+     * }
+     */
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            TreeNode node = root.left;
+            root.left = root.right;
+            root.right = node;
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            return root;
+        }
+    }
+}
