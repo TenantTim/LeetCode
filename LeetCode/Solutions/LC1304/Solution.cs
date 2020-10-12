2423ec7ffe6ae382d64f89d3b1631570639bb849
TenantTim <t-i.m@outlook.com>
2020-10-12 16:38:24 -0700

    Check in solution for 1304

---
+++ LeetCode/Solutions/LC1304/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solutions.LC1304
+{
+    public class Solution
+    {
+        public int[] SumZero(int n)
+        {
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = i - n / 2;
+            }
+
+            if (n % 2 == 0)
+            {
+                result[n - 1] += n / 2;
+            }
+
+            return result;
+        }
+    }
+}
