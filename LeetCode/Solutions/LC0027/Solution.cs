b4ab59b03db18d267908f28ab4d9467dd7e408b0
TenantTim <t-i.m@outlook.com>
2020-10-23 14:09:37 -0700

    Check in solution for LC27

---
+++ LeetCode/Solutions/LC0027/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solutions.LC0027
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[count] = nums[i];
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
